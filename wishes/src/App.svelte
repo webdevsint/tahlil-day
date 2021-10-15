e0647ccff00ad6e3bcfa1727d841413fcd89c72c
Nehan Yaser <67751528+webdevsint@users.noreply.github.com>
2021-10-15 16:39:45 +0600

    Added wish viewer

---
+++ wishes/src/App.svelte
@@ -0,0 +1,71 @@
+<script>
+  import Card from "./components/Card.svelte";
+
+  let wishes = [];
+
+  fetch("https://proxy.nehanyaser.repl.co/wisher?document=Tahlil")
+    .then((res) => res.json())
+    .then((data) => {
+      wishes = data;
+      document.querySelector(".loader").style.display = "none";
+    });
+</script>
+
+<main>
+  <header>
+    <h1>Happy Birthday, <span style="color: #3db2ff;">You!</span></h1>
+
+    <p>Here is a compilation of birthday wishes from everyone we could find.</p>
+  </header>
+
+  <div class="loader">
+    <div uk-spinner="ratio: 2" />
+  </div>
+
+  <main class="container">
+    <Card {wishes} />
+  </main>
+
+  <footer>
+    <center>
+      <h3 style="margin-bottom: 10px;">Made with Svelte, Vue and MangoDB</h3>
+      <a href="https://wish-tahlil.vercel.app/" target="blank"
+        >https://wish-tahlil.vercel.app/</a
+      >
+    </center>
+  </footer>
+</main>
+
+<style>
+  header {
+    padding: 30px;
+    padding-bottom: 0;
+  }
+  p {
+    margin: 20px 0;
+    margin-bottom: 0;
+  }
+  .loader {
+    min-height: calc(100vh - 365px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  footer {
+    background: #3db2ff;
+    color: white;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 0 10px;
+  }
+  h3,
+  a {
+    color: white;
+  }
+  a:hover {
+    color: white;
+  }
+</style>
---
+++ wishes/src/components/Card.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let wishes = [];
+
+  console.log(wishes);
+</script>
+
+{#each wishes as wish (wish.id)}
+  <div class="card">
+    <div class="from"><h3>{wish.wisher}</h3></div>
+    <hr />
+    <div class="wish">{wish.wish}</div>
+  </div>
+{/each}
+
+<style>
+  .card {
+    background: #fff;
+    border-radius: 2px;
+    display: inline-block;
+    margin: 1rem auto;
+    position: relative;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    padding: 30px;
+    width: 100%;
+  }
+  .from h3 {
+    color: rgb(61, 178, 255);
+  }
+  hr {
+    margin: 10px 0 20px 0;
+    opacity: 0.7;
+  }
+</style>
