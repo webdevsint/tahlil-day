0d7f0841b776e42e8326702633bf3b7d7b3352f3
Nehan Yaser <67751528+webdevsint@users.noreply.github.com>
2021-10-15 16:59:05 +0600

    Update commit

--- backend/backup.sh
+++ backend/backup.sh
@@ -2,4 +2,4 @@ cd documents
 curl -X POST \
     --data-binary @backup.json \
     --header "Content-Type:application/json" \
-    "https://www.filestackapi.com/api/store/S3?key=AqRJLx9EHSVC45bTXrbVaz"
+    "https://www.filestackapi.com/api/store/S3?key="
